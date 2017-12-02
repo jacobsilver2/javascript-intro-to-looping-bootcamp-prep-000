dbe53e806a7846839814578a1bf1081f2af7865e
Jacob Silver <jacobsilver2@mac.com>
2017-12-02 13:42:09 +0000

    automatically backed up by learn

---
+++ loops.js
@@ -0,0 +1,11 @@
+function forLoop(arr) {
+  for(i=0; i<25; i++) {
+    if (i===0) {
+      arr.push("I am 1 strange loop.")
+      } else {
+        arr.push(`I am ${i} strange loops.`)
+      }
+  }
+  return arr
+}
+
